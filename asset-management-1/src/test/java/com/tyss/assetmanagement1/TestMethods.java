01042e1a2824dd1b8e56ee2e7520fb47d71556cf
GitUserIndia1 <109454063+GitUserIndia1@users.noreply.github.com>
2022-10-01 01:45:46 +0530

    Update TestMethods.java

--- asset-management-1/src/test/java/com/tyss/assetmanagement1/TestMethods.java
+++ asset-management-1/src/test/java/com/tyss/assetmanagement1/TestMethods.java
@@ -50,6 +50,23 @@ class TestMethods {
 			if (assetTest1.size() == 1) {
 				assertEquals(assetTest1.get(0), assetTest);
 			}
+			
+			
+			int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
 		}
 	}
 
